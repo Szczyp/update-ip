fc1995ebdc27170988afb533d0e20136512665a6
Szczyp <qb@szczyp.com>
2014-02-16 20:23:32 +0100

    ENGAGE MONAD TRANSFORMERZ !!

--- Main.hs
+++ Main.hs
@@ -3,25 +3,144 @@
 
 module Main where
 
-import           ClassyPrelude
+import           ClassyPrelude                     hiding (log)
 import           Control.Error.Util
-import           Control.Lens                     hiding ((.=))
-import           Control.Lens.Aeson               (key, values, _String)
+import           Control.Lens                      hiding ((.=))
+import           Control.Lens.Aeson                (key, values, _String)
+import           Control.Monad.Trans.Either
+import           Control.Monad.Trans.Reader
 import           Control.Monad.Trans.State.Strict
+import           Control.Monad.Trans.Writer.Strict
 import           Data.Aeson
-import           Data.Aeson.Parser                (json')
-import           Data.Attoparsec.ByteString       (takeByteString)
-import           Data.Attoparsec.Char8            (char, decimal, parseOnly)
+import           Data.Aeson.Parser                 (json')
+import           Data.Attoparsec.ByteString        (takeByteString)
+import           Data.Attoparsec.Char8             (char, decimal, parseOnly)
+import           Data.Attoparsec.Types
+import           Data.ByteString.Lazy              (empty)
+import           Data.EitherR
+import           Network.HTTP.Client
 import           Network.HTTP.Types
-import           Pipes.Attoparsec                 (parse)
+import           Pipes.Attoparsec                  (parse)
 import           Pipes.HTTP
-import           Prelude                          (read)
+import           Prelude                           (read)
 import           System.Environment
+
+main :: IO ()
+main = do
+  config <- readConfig
+  either configError runApp config
+  where configError = const $ print "config error"
+        runApp config = runEitherT (execWriterT (runReaderT app config))
+                        >>= mapM_ print . either (return . pack) id
+
+type App = ReaderT AppConfig (WriterT [Text] (EitherT String IO))
+
+liftIOEither :: IO (Either String a) -> App a
+liftIOEither = liftIO >=> liftEither
+
+liftEither :: Either String a -> App a
+liftEither = lift . lift . hoistEither
+
+log :: Text -> App ()
+log t = lift $ tell [t]
+
+data AppConfig = AppConfig { configHeaders :: [Header]
+                           , apiURL        :: String
+                           , domain        :: String }
+                 deriving Read
+
+app :: App ()
+app = do
+  publicIP <- liftIOEither requestPublicIP
+  dns <- requestDNSRecord
+  ip <- liftEither $ getIP dns
+  if ip == publicIP
+    then do log "IP didn't change"
+    else do deleteDNSRecord dns
+            postDNSRecord publicIP
+            log $ "IP changed from: " ++ ipToText ip ++ " to: " ++ ipToText publicIP
+
+requestPublicIP :: IO (Either String IP)
+requestPublicIP = request "http://wtfismyip.com/text" methodGet [] emptyBody parseIP
+
+type DNSRecord = (Text, Text, Text, Text)
+
+requestDNSRecord :: App [DNSRecord]
+requestDNSRecord = do
+  domain <- pack . domain <$> ask
+  withAPI methodGet emptyBody >>= return . filter (predicate domain) . records
+  where predicate domain (_, t, n, _) = (t, n) == ("A", domain)
+        records = zip4
+                  <$> scope "content"
+                  <*> scope "type"
+                  <*> scope "name"
+                  <*> scope "record_id"
+        scope k = toListOf $ key "records" . values . key k . _String
+
+deleteDNSRecord :: [DNSRecord] -> App Text
+deleteDNSRecord dns = withAPI methodDelete body >>= return . message
+  where body = RequestBodyLBS
+               . encode
+               . object
+               $ [("record_id", String (dns ^. _head . _4))]
+
+postDNSRecord :: IP -> App Text
+postDNSRecord ip = withAPI methodPost body >>= return . message
+  where body = RequestBodyLBS
+               . encode
+               . object
+               $ [("type", String "A")
+                 ,("hostname" , String "")
+                 ,("content", String . ipToText $ ip)
+                 ,("ttl", Number 300)]
+
+withAPI :: ByteString -> RequestBody -> App Value
+withAPI method body = do
+  config <- ask
+  url <- liftEither . note "invalid method" $ getUrl config
+  liftIOEither $ request
+    url
+    method
+    ([("Content-type", "application/json")] ++ configHeaders config)
+    body
+    json'
+  where getUrl config = do
+          m <- lookup method methodMap
+          return $ apiURL config ++ m ++ "/" ++ domain config
+        methodMap :: Map Method String
+        methodMap = mapFromList [(methodDelete, "delete")
+                                ,(methodPost, "create")
+                                ,(methodGet, "list")]
+
+message :: Value -> Text
+message = view $ key "result" . key "message" . _String
+
+request :: String
+        -> Method
+        -> [Header]
+        -> RequestBody
+        -> Parser ByteString a
+        -> IO (Either String a)
+request url method headers body parser = do
+  r <- parseUrl url
+  let req = r { method = method, requestHeaders = headers, requestBody = body }
+  withManager tlsManagerSettings $ \manager ->
+    withHTTP req manager $ \response ->
+      evalStateT (parse parser) (responseBody response)
+  >>= return . fmapL (const $ "Error parsing " ++ url)
+
+getIP :: [DNSRecord] -> Either String IP
+getIP = parseOnly parseIP . encodeUtf8 . view (_head . _1)
+
+emptyBody :: RequestBody
+emptyBody = RequestBodyLBS empty
 
 data IP = IP Word8 Word8 Word8 Word8 deriving (Show, Eq)
 
-toText (IP a b c d) = pack . join . intersperse "." . map show $ [a, b, c, d]
+ipToText :: IP -> Text
+ipToText (IP a b c d) = pack . join . intersperse "." . map show $ [a, b, c, d]
 
+parseIP :: Parser ByteString IP
 parseIP = do
   a <- decimal
   char '.'
@@ -32,84 +151,11 @@ parseIP = do
   d <- decimal
   return $ IP a b c d
 
-request url method headers body parser = do
-  r <- parseUrl url
-  let req = r { requestHeaders = headers, method = method }
-  let req' = case body of
-        Nothing -> req
-        Just body -> req { requestBody = body }
-  withManager tlsManagerSettings $ \manager ->
-    withHTTP req' manager $ \response ->
-      evalStateT (parse parser) (responseBody response)
-
-requestPublicIP = request "http://wtfismyip.com/text" methodGet [] Nothing parseIP
-
-requestDNSRecord headers = filter predicate . records <$> json
-  where predicate (_, t, n, _) = (t, n) == ("A", "szczyp.com")
-        records = zip4
-                  <$> scope "content"
-                  <*> scope "type"
-                  <*> scope "name"
-                  <*> scope "record_id"
-        scope k = toListOf $ _Right
-                  . key "records"
-                  . values
-                  . key k
-                  . _String
-        json = request "https://api.name.com/api/dns/list/szczyp.com" methodGet headers Nothing json'
-
-deleteDNSRecord headers dns = select <$> result
-  where select = toListOf $ _Right . key "result" . key "message" . _String
-        result = request
-                 "https://api.name.com/api/dns/delete/szczyp.com"
-                 methodDelete
-                 ([("Content-type", "application/json")] ++ headers)
-                 body
-                 json'
-        body = Just
-               . RequestBodyLBS
-               . encode
-               . object
-               $ [("record_id", String (dns ^. _head . _4))]
-
-postDNSRecord headers ip = result
-  where result = request
-                 "https://api.name.com/api/dns/create/szczyp.com"
-                 methodPost
-                 ([("Content-type", "application/json")] ++ headers)
-                 body
-                 json'
-        body = Just
-               . RequestBodyLBS
-               . encode
-               . object
-               $ [("type", String "A")
-                 ,("hostname" , String "")
-                 ,("content", String . toText $ ip)
-                 ,("ttl", Number 300)]
-
-getIP = parseOnly parseIP . encodeUtf8 . view (_head . _1)
-
-readHeaders = tryAnyDeep $ do
+readConfig :: IO (Either SomeException AppConfig)
+readConfig = tryAny $ do
   root <- getExecutablePath
-  contents <- readFile $ directory (fpFromString root) </> fpFromText "headers"
+  contents <- readFile $ directory (fpFromString root) </> fpFromText "config"
   return $ read contents
 
-main = do
-  headers <- readHeaders
-  case headers of
-    Left _ -> print "can't parse headers for api username and token"
-    Right headers -> do
-      publicIP <- requestPublicIP
-      dns <- requestDNSRecord headers
-      case (publicIP, getIP dns) of
-        (Right publicIP, Right ip) | not $ ip == publicIP ->
-            do print "IP changed"
-               print "deleting dns record"
-               deleteDNSRecord headers dns
-               print "dns record deleted"
-               print "creating new dns record"
-               postDNSRecord headers publicIP
-               print "new dns record created"
-
-        _ -> print "IP didn't change or cannot parse public ip address or cannot parse dns record"
+readConfig' :: IO (Either SomeException AppConfig)
+readConfig' = tryAny $ read <$> readFile "config"
